eb86dae3500976239c3b8cd024375ae39eabd8c1
Roland Darvas <darvas.roland@gmail.com>
2018-03-07 15:26:05 +0100

    first commit

---
+++ kivkez/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kivkez
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
